a5feda441f679ac0799d05b7d1ab16f725301a90
TanmayMachkar <tanmaymacho@gmail.com>
2024-09-27 23:03:30 +0530

    last 2 parts will be done tmrw

--- Matplotlib/Scatter Plots.py
+++ Matplotlib/Scatter Plots.py
@@ -1,25 +1,32 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 
-plt.style.use('seaborn')
+plt.style.use('fivethirtyeight')
 
-x = [5, 7, 8, 5, 6, 7, 9, 2, 3, 4, 4, 4, 2, 6, 3, 6, 8, 6, 4, 1]
-y = [7, 4, 3, 9, 1, 3, 2, 5, 2, 4, 8, 7, 1, 6, 4, 9, 7, 7, 5, 1]
-
-
+# x = [5, 7, 8, 5, 6, 7, 9, 2, 3, 4, 4, 4, 2, 6, 3, 6, 8, 6, 4, 1]
+# y = [7, 4, 3, 9, 1, 3, 2, 5, 2, 4, 8, 7, 1, 6, 4, 9, 7, 7, 5, 1]
 # colors = [7, 5, 9, 7, 5, 7, 2, 5, 3, 7, 1, 2, 8, 1, 9, 2, 5, 6, 7, 5]
+# sizes = [209, 486, 381, 255, 191, 315, 185, 228, 174, 538, 239, 394, 399, 153, 273, 293, 436, 501, 397, 539]
+# cbar = plt.colorbar()
+# cbar.set_label("Satifaction")
 
-# sizes = [209, 486, 381, 255, 191, 315, 185, 228, 174,
-#          538, 239, 394, 399, 153, 273, 293, 436, 501, 397, 539]
+data = pd.read_csv(r'C:\Users\ADMIN\AI-ML\Matplotlib\2019-05-31-data.csv')
+view_count = data['view_count']
+likes = data['likes']
+ratio = data['ratio']
 
-# data = pd.read_csv('2019-05-31-data.csv')
-# view_count = data['view_count']
-# likes = data['likes']
-# ratio = data['ratio']
+plt.scatter(view_count, likes, c = ratio, cmap = "summer", edgecolor = "black", linewidth = 1, alpha = 0.75)
 
-# plt.title('Trending YouTube Videos')
-# plt.xlabel('View Count')
-# plt.ylabel('Total Likes')
+cbar = plt.colorbar()
+cbar.set_label("Like/Dislike Ratio")
+
+# use log scale to lessen how much outliers skew the plot
+plt.xscale("log")
+plt.yscale("log")
+
+plt.title('Trending YouTube Videos')
+plt.xlabel('View Count')
+plt.ylabel('Total Likes')
 
 plt.tight_layout()
 
